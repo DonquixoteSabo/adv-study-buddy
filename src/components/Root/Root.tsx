f02073b8a83fabb7a9ecf4ebac0dec942b5e62e1
DonquixoteSabo <mixboksu@gmail.com>
2022-03-09 18:36:07 +0100

    build: Add CSS normalize

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,6 +1,10 @@
+//styled-components setup
+import GlobalStyle from 'assets/styles/GlobalStyle';
+
 const Root = () => {
   return (
     <>
+      <GlobalStyle />
       <h1>HELLO, Have a nice coding bro {'<3'} Glad u are here</h1>
     </>
   );
