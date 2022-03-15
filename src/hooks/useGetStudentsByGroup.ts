f7f662e3cde973c7938d5c5030c7c93a4b3c2063
DonquixoteSabo <mixboksu@gmail.com>
2022-03-15 18:31:54 +0100

    chore: Add useGetStudentsByGroup hook
    Add hook that allows to fetch student by group

---
+++ src/hooks/useGetStudentsByGroup.ts
@@ -0,0 +1,34 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+export const ENDPOINT = 'http://localhost:5000/groups/';
+
+interface Query {
+  data: { students: Student[] };
+}
+
+interface Subject {
+  subject: string;
+  average: number;
+}
+
+export interface Student {
+  id: { $iod: string };
+  firstName: string;
+  lastName: string;
+  attendance: number;
+  average: number;
+  group: string;
+  course: string;
+  grades: Subject[];
+}
+
+const useGetStudentsByGroup = (group: string) => {
+  return useQuery<Query>(['students', group], async () => {
+    const data = await axios.get(ENDPOINT + group);
+
+    return data.data.students;
+  });
+};
+
+export { useGetStudentsByGroup };
