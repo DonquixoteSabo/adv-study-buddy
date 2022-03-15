25ee47b88e66b7857ab5dc2125d21352171682ba
DonquixoteSabo <mixboksu@gmail.com>
2022-03-15 18:32:10 +0100

    chore: Fetch students in StudentsSection

--- src/components/templates/StudentsSection/StudentsSection.tsx
+++ src/components/templates/StudentsSection/StudentsSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useGetStudentsByGroup } from 'hooks/useGetStudentsByGroup';
 
 const Wrapper = styled.section`
   grid-column: 2/3;
@@ -7,8 +8,19 @@ const Wrapper = styled.section`
 `;
 
 const StudentsSection = () => {
+  const { isLoading, error, data } = useGetStudentsByGroup('A');
 
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
 
+  if (error) {
+    console.log(data);
+    return <h1>Sorry, but we couldn't load data for you</h1>;
+  }
+
+  console.log(data);
+ 
   return (
     <Wrapper>
       <p>Students list</p>
