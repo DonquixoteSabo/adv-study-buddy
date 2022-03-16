f3ec00c05e04aa03f963c666ff9e3718f4fced85
DonquixoteSabo <mixboksu@gmail.com>
2022-03-16 06:38:44 +0100

    chore: Style StudentsList Wrapper

--- src/components/templates/StudentsSection/StudentsSection.tsx
+++ src/components/templates/StudentsSection/StudentsSection.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useGetStudentsByGroup } from 'hooks/useGetStudentsByGroup';
+import { Student, useGetStudentsByGroup } from 'hooks/useGetStudentsByGroup';
 
 const Wrapper = styled.section`
   grid-column: 2/3;
   grid-row: 2/-1;
+  padding: ${({ theme }) => theme.spacing.l};
 `;
 
+const StyledList = styled.ul`
+  background: #fff;
+  list-style: none;
+  max-width: 840px;
+  padding: ${({ theme }) => theme.spacing.m};
+`;
+
+const StyledItem = styled.li`
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  grid-template-rows: repeat(2, 1.6rem);
+`;
+
+const StyledGrade = styled.div``;
+
+const StyledName = styled.p``;
+const StyledAttendance = styled.small``;
+const StyledDeleteButton = styled.button``;
 const StudentsSection = () => {
   const { isLoading, error, data } = useGetStudentsByGroup('A');
 
@@ -20,10 +39,22 @@ const StudentsSection = () => {
   }
 
   console.log(data);
- 
+
   return (
     <Wrapper>
       <p>Students list</p>
+      <StyledList>
+        {data!.map((student: Student) => (
+          <StyledItem key={student.lastName + student.firstName}>
+            <StyledGrade>{student.average}</StyledGrade>
+            <StyledName>
+              {student.lastName} {student.firstName}
+            </StyledName>
+            <StyledAttendance>{student.attendance}</StyledAttendance>
+            <StyledDeleteButton>X</StyledDeleteButton>
+          </StyledItem>
+        ))}
+      </StyledList>
     </Wrapper>
   );
 };
