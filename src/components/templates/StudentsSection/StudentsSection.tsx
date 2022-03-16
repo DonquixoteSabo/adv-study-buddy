662996a08aad0890b4e4c9a60ddff7476ad64ec6
DonquixoteSabo <mixboksu@gmail.com>
2022-03-16 07:08:30 +0100

    chore: Style Students List

--- src/components/templates/StudentsSection/StudentsSection.tsx
+++ src/components/templates/StudentsSection/StudentsSection.tsx
@@ -13,12 +13,23 @@ const StyledList = styled.ul`
   list-style: none;
   max-width: 840px;
   padding: ${({ theme }) => theme.spacing.m};
+  color: ${({ theme }) => theme.colors.primary};
+  overflow-y: scroll;
+  height: 70vh;
 `;
 
 const StyledItem = styled.li`
+  border-bottom: 1px solid #dfe2e8;
+  //padding: ${({ theme }) => theme.spacing.m} 0;
+  padding: 10px 0;
+`;
+
+const GridWrapper = styled.div`
+  width: 400px;
   display: grid;
-  grid-template-columns: repeat(3, auto);
+  grid-template-columns: auto 1fr auto;
   grid-template-rows: repeat(2, 1.6rem);
+  grid-gap: 10px;
 `;
 
 interface GradeProps {
@@ -28,6 +39,7 @@ interface GradeProps {
 const StyledGrade = styled.div<GradeProps>`
   grid-row: 1/-1;
   grid-column: 1/2;
+  align-self: center;
   border-radius: 50%;
   width: 44px;
   height: 44px;
@@ -50,9 +62,32 @@ const StyledGrade = styled.div<GradeProps>`
   color: white;
 `;
 
-const StyledName = styled.p``;
-const StyledAttendance = styled.small``;
-const StyledDeleteButton = styled.button``;
+const StyledName = styled.p`
+  font-weight: 700;
+  font-size: ${({ theme }) => theme.fontSize.m};
+  display: flex;
+  align-items: center;
+  margin-left: 1em;
+`;
+const StyledAttendance = styled.small`
+  display: flex;
+  align-items: center;
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  margin-left: 1em;
+`;
+
+const StyledDeleteButton = styled.button`
+  grid-column: 3/4;
+  grid-row: 1/2;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  border: none;
+  background: #c0c7d6;
+  color: white;
+  font-size: ${({ theme }) => theme.fontSize.s};
+`;
+
 const StudentsSection = () => {
   const { isLoading, error, data } = useGetStudentsByGroup('A');
 
@@ -73,14 +108,18 @@ const StudentsSection = () => {
       <StyledList>
         {students?.map((student: Student) => (
           <StyledItem key={student.lastName + student.firstName}>
-            <StyledGrade average={student.average}>
-              {student.average}
-            </StyledGrade>
-            <StyledName>
-              {student.lastName} {student.firstName}
-            </StyledName>
-            <StyledAttendance>{student.attendance}</StyledAttendance>
-            <StyledDeleteButton>X</StyledDeleteButton>
+            <GridWrapper>
+              <StyledGrade average={student.average}>
+                {student.average}
+              </StyledGrade>
+              <StyledName>
+                {student.lastName} {student.firstName}
+              </StyledName>
+              <StyledAttendance>
+                attendance: {student.attendance}%
+              </StyledAttendance>
+              <StyledDeleteButton>X</StyledDeleteButton>
+            </GridWrapper>
           </StyledItem>
         ))}
       </StyledList>
