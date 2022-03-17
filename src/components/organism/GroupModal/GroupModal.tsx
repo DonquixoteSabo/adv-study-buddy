dfdb31b6a1bedd39391b8cd580adb9a7467f89ed
DonquixoteSabo <mixboksu@gmail.com>
2022-03-17 07:28:57 +0100

    chore: Style and handle Group Modal logic

--- src/components/organism/GroupModal/GroupModal.tsx
+++ src/components/organism/GroupModal/GroupModal.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useGetAllGroups } from '../../../hooks/useGetAllGroups';
 
-const Wrapper = styled(Modal)`
+const StyledModal = styled(Modal)`
+  width: 300px;
+  padding: 25px 0;
   background: white;
-  width: auto;
-  height: auto;
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+const Header = styled.header`
+  font-family: ${({ theme }) => theme.fontFamily.roboto};
+  font-size: ${({ theme }) => theme.fontSize.l};
+  font-weight: 700;
+  padding: ${({ theme }) => theme.spacing.m} 0;
+  text-align: center;
+`;
+
+const Item = styled(Link)`
+  display: block;
+  text-decoration: none;
+  text-align: center;
+  padding: 1em 0;
+  margin: 0 ${({ theme }) => theme.spacing.l};
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    background: #eceff7;
+  }
 `;
 
 interface Props {
@@ -14,10 +47,32 @@ interface Props {
 }
 
 const GroupModal = ({ isOpen, closeModal }: Props) => {
+  const { isLoading, error, data } = useGetAllGroups();
+
   return (
-    <Wrapper isOpen={isOpen} contentLabel="Select group">
-      <h1>Select a group</h1>A B C<button onClick={closeModal}>CLose</button>
-    </Wrapper>
+    <StyledModal
+      isOpen={isOpen}
+      contentLabel="Select group"
+      onRequestClose={closeModal}
+      style={{
+        overlay: { backgroundColor: ' rgba(115, 124, 142, 0.4)' },
+      }}
+    >
+      <Header>Select a group</Header>
+      {error ? (
+        <h4>Error</h4>
+      ) : isLoading ? (
+        <h4>Loading...</h4>
+      ) : (
+        <div>
+          {data?.data.groups.map((group) => (
+            <Item key={group} to={`/students/${group}`} onClick={closeModal}>
+              {group}
+            </Item>
+          ))}
+        </div>
+      )}
+    </StyledModal>
   );
 };
 
