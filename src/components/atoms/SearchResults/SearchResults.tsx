7b950855b09a33a7158262fe9c8a4054b3ab059d
DonquixoteSabo <mixboksu@gmail.com>
2022-03-23 14:13:31 +0100

    chore: Handle finding student in API

--- src/components/atoms/SearchResults/SearchResults.tsx
+++ src/components/atoms/SearchResults/SearchResults.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import { useFindStudents } from 'hooks/useFindStudents';
+
 interface Props {
   results: string;
 }
 
 const SearchResults = ({ results }: Props) => {
-  console.log(results);
-  return <div></div>;
+  const { data } = useFindStudents(results);
+  const students = data?.data.students;
+  console.log(students);
+
+  if (students && students.length > 0) {
+    return (
+      <div>
+        {students.map((student) => (
+          <li key={student.id.$oid}>{student.firstName}</li>
+        ))}
+      </div>
+    );
+  }
+
+  return <div />;
 };
 
 export default SearchResults;
