9c256ac6d52d1db171d51a8452bc094629783a5e
DonquixoteSabo <mixboksu@gmail.com>
2022-03-31 18:10:05 +0200

    feat: Add datapicker

--- src/components/organism/ClassForm/ClassForm.tsx
+++ src/components/organism/ClassForm/ClassForm.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import styled from 'styled-components';
 import ClassHeader from 'components/atoms/ClassHeader/ClassHeader';
 import { StyledInput } from '../../atoms/StyledInput/StyledInput';
 import { StyledTextArea } from '../../atoms/StyledTextArea/StyledTextArea';
 import FormButton from '../../atoms/FormButton/FormButton';
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 type Inputs = {
   subject: string;
@@ -22,12 +24,20 @@ export const Wrapper = styled.div`
   display: grid;
 `;
 
+export const StyledDatePicker = styled(ReactDatePicker)`
+  border: 1px solid #c0c7d6;
+  box-shadow: -2px 4px 10px rgba(115, 124, 142, 0.09);
+  border-radius: 25px;
+  padding: 0.5em;
+`;
+
 const ClassForm = () => {
-  const { register, handleSubmit, reset } = useForm<Inputs>();
+  const { register, handleSubmit, reset, control } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data);
     reset();
   };
+  // @ts-ignore
   return (
     <>
       <ClassHeader>Schedule an exam</ClassHeader>
@@ -35,14 +45,27 @@ const ClassForm = () => {
         <Wrapper>
           {/*SUBJECT*/}
           <StyledLabel>Subject</StyledLabel>
-          <StyledInput {...register('subject')} />
+          <StyledInput
+            {...register('subject', { required: true, maxLength: 25 })}
+          />
           {/*DATE*/}
           <StyledLabel>Date</StyledLabel>
-          <StyledInput placeholder="DD/MM-hour" {...register('date')} />
+          {/*<StyledInput placeholder="DD/MM-hour" {...register('date')} />*/}
+          <Controller
+            control={control}
+            name="date"
+            render={({ field }) => (
+              <StyledDatePicker
+                onChange={(date: any) => field.onChange(date)}
+                onBlur={field.onBlur}
+                selected={field.value || new Date()}
+              />
+            )}
+          />
           {/*DESCRIPTION*/}
           <StyledLabel>Description</StyledLabel>
           <StyledTextArea
-            {...register('description')}
+            {...register('description', { max: 99 })}
             style={{ height: 140 }}
           />
           <FormButton type="submit">Confirm</FormButton>
