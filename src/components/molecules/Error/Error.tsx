a9e4c9afe8a5d3da8dbb77e88a546959eafef469
DonquixoteSabo <mixboksu@gmail.com>
2022-04-11 15:38:02 +0200

    chore: Delete error background
    This solution wasn't user friendly

--- src/components/molecules/Error/Error.tsx
+++ src/components/molecules/Error/Error.tsx
@@ -1,47 +1,13 @@
 import React, { useContext } from 'react';
-import styled from 'styled-components';
 
 import { ErrorContext } from 'helpers/ErrorContext';
+
+import { Wrapper } from './Error.styles';
 
 interface Props {
   title?: string;
   msg?: string;
 }
-
-interface ErrorProps {
-  error: boolean;
-}
-
-export const Wrapper = styled.div<ErrorProps>`
-  padding: ${({ theme }) => theme.spacing.m};
-  border: 3px solid #ff8383;
-  border-radius: 15px;
-  position: absolute;
-  bottom: 10%;
-  background: white;
-  transition: 1s ease-in-out;
-  transform: translateX(-50%);
-  left: ${({ error }) => (error ? '50%' : '-200%')};
-  z-index: 10;
-
-  header {
-    font-weight: 700;
-    font-size: 20px;
-  }
-
-  color: #ff8383;
-  font-family: ${({ theme }) => theme.fontFamily.roboto};
-`;
-
-export const Background = styled.div<ErrorProps>`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(115, 124, 142, 0.1);
-  z-index: ${({ error }) => (error ? '5' : '-20')};
-`;
 
 const Error = ({
   title = 'Oops!',
@@ -49,13 +15,10 @@ const Error = ({
 }: Props) => {
   const { error } = useContext(ErrorContext);
   return (
-    <>
-      <Background error={!!error} />
-      <Wrapper error={!!error}>
-        <header>{title}</header>
-        <p>{msg}</p>
-      </Wrapper>
-    </>
+    <Wrapper error={!!error}>
+      <header>{title}</header>
+      <p>{msg}</p>
+    </Wrapper>
   );
 };
 
